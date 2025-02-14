d9dcbd58495a04b2a68cd47d006ab76c56a0f9d9
Asad <asadalipuh5@gmail.com>
2025-02-15 04:50:25 +0500

    Conditional Rendering and Rendering List

--- Conditional Rendering and rendering lists/src/App.jsx
+++ Conditional Rendering and rendering lists/src/App.jsx
@@ -5,8 +5,31 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [showbtn, setshowbtn] = useState(false)
+  const [showbtn, setshowbtn] = useState(true)
+  const [todos, settodos] = useState([
+    {
+      title: "hey",
+      desc: "I am a good todo"
+    },
+    {
+      title: "hey Another todo",
+      desc: "I am a good Another todo"
+    },
+    {
+      title: "hey yet Another todo",
+      desc: "I am a good yet Another todo"
+    },
 
+  ])
+
+  const Todo = ({ todo }) => {
+    return (<>
+    <div className="m-4 border border-1 border-red">
+      <div className="todo">{todo.title}</div>
+      <div className="todo">{todo.desc}</div>
+      </div>
+    </>)
+  }
   return (
     <>
       <div>
@@ -18,10 +41,15 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      {/* {showbtn?<button>I will be shown only when second button is clicked</button>:"thengaa"} */}
+      {showbtn ? <button>showbtn is true</button> : <button>showbtn is true</button>}
       {/* Codintional Rendering */}
-      {showbtn && <button>I will be shown only when second button is clicked</button>}
+      {/* {showbtn && <button>I will be shown only when second button is clicked</button>} */}
       <div className="card">
+        
+        {/* Rendering Lists */}
+        {todos.map((todo)=>{
+          return <Todo key = {todo.titletitle} todo={todo}/>
+        })}
         <button onClick={() => setshowbtn(!showbtn)}>
           count is {count}
         </button>
